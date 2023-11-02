21c8f6fc9ae03b80b77240b28eb2fd7247919d9f
igor <imor2703@g.educaand.es>
2023-11-02 09:00:36 +0100

    hola usa la clase HolaMundo

--- lib/hola.php
+++ lib/hola.php
@@ -1,6 +1,8 @@
 <?php
 // Autor: Igor imor2703@g.educaand.es
-print "Introduce tu nombre:";
+require('HolaMundo.php');
+
+print "Introduce tu nombre: ";
 $nombre = trim(fgets(STDIN));
-@print "Hola, {$nombre}\n";
+print new HolaMundo($nombre);
 ?>
